652ebaa5ed51df11f9387b8138653db45437ef9f
Mikkel Alexander Madsen <me@mikkel.am>
2018-08-27 17:11:33 +0200

    changes if contact is not available

--- webwhatsapi/objects/whatsapp_object.py
+++ webwhatsapi/objects/whatsapp_object.py
@@ -71,7 +71,7 @@ class WhatsappObjectWithId(WhatsappObject):
         :type driver: WhatsAPIDriver
         """
         super(WhatsappObjectWithId, self).__init__(js_obj, driver)
-        if 'id' in js_obj:
+        if 'id' in js_obj and js_obj['id']:
             if type(js_obj["id"]) is str:
                 self.id = js_obj["id"]
             else:
